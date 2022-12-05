c692c824cdf7d26cd78557da28833736e52b73a4
Guy <guyelizarov@gmail.com>
2022-12-05 22:14:12 +0200

    finish header layout

--- src/cmps/shared/app-header.tsx
+++ src/cmps/shared/app-header.tsx
@@ -3,7 +3,7 @@ import { useAppDispatch, useAppSelector } from "../../store/hooks"
 import { logout } from "../../store/user/user.actions"
 import { selectUser } from "../../store/user/user.slice"
 
-
+import logo from "../../assets/imgs/logo.png"
 
 export const AppHeader = () => {
 
@@ -16,8 +16,14 @@ export const AppHeader = () => {
     return <header className='main-layout '>
         <div className="header-container flex space-between">
 
-            <div className="header-right-container">
-
+            <div className="header-right-container flex center">
+                <div className="header-right-container-logo">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="header-right-container-txt flex column space-between">
+                    <h1>גלבוע אוהבת חיות</h1>
+                    <span>היי אורח! התחבר</span>
+                </div>
             </div>
             <nav className="header-left-container flex">
                 <NavLink to={'/'}>אודות</NavLink>
