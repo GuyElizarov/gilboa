39b60833dec739b1b074825725e9508fcccd8234
Guy <guyelizarov@gmail.com>
2022-12-06 19:37:13 +0200

    adding cards styling

--- src/cmps/home/questionnaire.tsx
+++ src/cmps/home/questionnaire.tsx
@@ -7,10 +7,10 @@ export const Questionnaire = () => {
             <span>נשארו לנו עוד כמה שאלות קטנות לפני שתוכלו לפגוש את החבר הכי טוב החדש שלכם 🐶.</span>
             <span>אל תדאגו - תמיד אפשר לשנות את התשובות שלכם בדף X (או לחזור אחורה).</span>
         </div>
-        <div className="questionnaire-bottom-container">
-            <div className="questionnaire-card"></div>
-            <div className="questionnaire-card"></div>
-            <div className="questionnaire-card"></div>
+        <div className="questionnaire-bottom-container flex">
+            <div className="questionnaire-card app-card"></div>
+            <div className="questionnaire-card app-card"></div>
+            <div className="questionnaire-card app-card"></div>
         </div>
 
     </div>
