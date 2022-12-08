77226b37edec074207f604ac09d9009c94653f47
Guy <guyelizarov@gmail.com>
2022-12-08 12:08:17 +0200

    adding 'dog' JSON'

---
+++ src/data/dog.js
@@ -0,0 +1,25 @@
+const dog = [
+    {
+        _id: "HF645FH",
+        name: 'puki',
+        imgSrc: {
+            thumbnail: "sdfsdfsdf",
+            imgs: ["dsfgdsfg", "sdfsdfsdf"]
+        },
+        age: 2,
+        sex: 'male',
+        weightKG: 8,
+        bread: 'pug',
+        isDangerous: false,
+        reqTimesOutside: 3,
+        isAttached: [
+            // otherDogsId 
+            "KFDK656"],
+        adoptionCriteria: {
+            backyard: "fenced",
+            aloneHoursCapability: 7,
+            livingEnv: ['backyardOrPorch', 'house', 'backyardOrPorchSleepAtHome']
+        }
+    },
+
+]
