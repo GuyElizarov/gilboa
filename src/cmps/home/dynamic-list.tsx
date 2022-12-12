868f6f2eed7a0468b21e739ea7141112eda5954a
Eytan Silberberg <eytan@Eytans-MacBook-Pro.local>
2022-12-12 17:56:48 +0200

    building dynamic list

--- src/cmps/home/dynamic-list.tsx
+++ src/cmps/home/dynamic-list.tsx
@@ -1,6 +1,6 @@
 import { Condition, PreviewTypes, QuestionnaireCard } from "../../interface/home/home-interface";
 import { Dog } from "../../interface/shared/dogs-interface";
-import { ConditionPreview } from "./condition-preview";
+// import { FirstStagePreview } from "./condition-preview";
 
 export const DynamicList = ({ type, content }: { type: PreviewTypes, content: Condition[] | QuestionnaireCard[] | Dog[] }) => {
     // switch (type){
@@ -13,7 +13,6 @@ export const DynamicList = ({ type, content }: { type: PreviewTypes, content: Co
     //         return <div className=""></div>
     // }
     return <div></div>
-
 
 
 }
